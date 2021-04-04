fea3a8521fee34ead7e4cf80fc1ed28cec759f0d
Nurullah YAYAN <nurullah.yayan@gmail.com>
2021-04-04 04:28:24 +0300

    bugfix and validation message dynamic change

--- nyfileplugin.js
+++ nyfileplugin.js
@@ -7,6 +7,29 @@
     NY.FilePlugin = { config: {}, files: [] };
   }
 
+  NY.FilePlugin.config.byteUnits = [
+    " kB",
+    " MB",
+    " GB",
+    " TB",
+    "PB",
+    "EB",
+    "ZB",
+    "YB",
+  ];
+
+  NY.FilePlugin.config.icons = {
+    cancel:
+      '<svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 512.001 512.001" style="width:18px; height:18px;" xml:space="preserve"> <path d="M405.6,69.6C360.7,24.7,301.1,0,237.6,0s-123.1,24.7-168,69.6S0,174.1,0,237.6s24.7,123.1,69.6,168s104.5,69.6,168,69.6s123.1-24.7,168-69.6s69.6-104.5,69.6-168S450.5,114.5,405.6,69.6z M386.5,386.5c-39.8,39.8-92.7,61.7-148.9,61.7s-109.1-21.9-148.9-61.7c-82.1-82.1-82.1-215.7,0-297.8C128.5,48.9,181.4,27,237.6,27s109.1,21.9,148.9,61.7C468.6,170.8,468.6,304.4,386.5,386.5z"/> <path d="M342.3,132.9c-5.3-5.3-13.8-5.3-19.1,0l-85.6,85.6L152,132.9c-5.3-5.3-13.8-5.3-19.1,0c-5.3,5.3-5.3,13.8,0,19.1l85.6,85.6l-85.6,85.6c-5.3,5.3-5.3,13.8,0,19.1c2.6,2.6,6.1,4,9.5,4s6.9-1.3,9.5-4l85.6-85.6l85.6,85.6c2.6,2.6,6.1,4,9.5,4c3.5,0,6.9-1.3,9.5-4c5.3-5.3,5.3-13.8,0-19.1l-85.4-85.6l85.6-85.6C347.6,146.7,347.6,138.2,342.3,132.9z"/> </svg>',
+  };
+
+  NY.FilePlugin.config.messages = {
+    fileSizeErrorMessage: "Tekli dosya seçim boyutunu aştınız",
+    totalSizeErrorMessage: "Maximum yükleyebileceğiniz boyutu aştınız",
+    extensionErrorMessage:
+      "Seçtiğiniz dosyaların uzantısı desteklenmemektedir.",
+  };
+
   window.onload = function () {
     var fileInputs = document.querySelectorAll(".ny-file");
     var key = 100001;
@@ -33,7 +56,7 @@
 
       if (item.dataset.totalSize !== undefined) {
         var totalSize = parseInt(item.dataset.totalSize);
-        if (size) {
+        if (totalSize) {
           prop.totalSize = totalSize;
         }
       }
@@ -41,6 +64,22 @@
       if (item.accept !== undefined) {
         prop.mimeTypes = item.accept.split(",");
       }
+
+      //Messages
+      if (item.dataset.fileSizeErrorMessage !== undefined) {
+        NY.FilePlugin.config.messages.fileSizeErrorMessage =
+          item.dataset.fileSizeErrorMessage;
+      }
+      if (item.dataset.totalSizeErrorMessage !== undefined) {
+        NY.FilePlugin.config.messages.totalSizeErrorMessage =
+          item.dataset.totalSizeErrorMessage;
+      }
+
+      if (item.dataset.extensionErrorMessage !== undefined) {
+        NY.FilePlugin.config.messages.extensionErrorMessage =
+          item.dataset.extensionErrorMessage;
+      }
+      //Messages
 
       NY.FilePlugin.files.push(prop);
       item.addEventListener("change", function (e) {
@@ -155,15 +194,21 @@
 
         if (data.errors.extension) {
           errHtml +=
-            "<li>" + NY.FilePlugin.config.messages.extensionError + "</li>";
+            "<li>" +
+            NY.FilePlugin.config.messages.extensionErrorMessage +
+            "</li>";
         }
         if (data.errors.fileSize) {
           errHtml +=
-            "<li>" + NY.FilePlugin.config.messages.fileSizeError + "</li>";
+            "<li>" +
+            NY.FilePlugin.config.messages.fileSizeErrorMessage +
+            "</li>";
         }
         if (data.errors.totalFilesSize) {
           errHtml +=
-            "<li>" + NY.FilePlugin.config.messages.totalFileSizeError + "</li>";
+            "<li>" +
+            NY.FilePlugin.config.messages.totalSizeErrorMessage +
+            "</li>";
         }
 
         errorEl.innerHTML = errHtml;
@@ -251,27 +296,5 @@
       } while (size > 1024);
       return Math.max(size, 0.1).toFixed(2) + NY.FilePlugin.config.byteUnits[i];
     }
-
-    NY.FilePlugin.config.byteUnits = [
-      " kB",
-      " MB",
-      " GB",
-      " TB",
-      "PB",
-      "EB",
-      "ZB",
-      "YB",
-    ];
-
-    NY.FilePlugin.config.icons = {
-      cancel:
-        '<svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 512.001 512.001" style="width:18px; height:18px;" xml:space="preserve"> <path d="M405.6,69.6C360.7,24.7,301.1,0,237.6,0s-123.1,24.7-168,69.6S0,174.1,0,237.6s24.7,123.1,69.6,168s104.5,69.6,168,69.6s123.1-24.7,168-69.6s69.6-104.5,69.6-168S450.5,114.5,405.6,69.6z M386.5,386.5c-39.8,39.8-92.7,61.7-148.9,61.7s-109.1-21.9-148.9-61.7c-82.1-82.1-82.1-215.7,0-297.8C128.5,48.9,181.4,27,237.6,27s109.1,21.9,148.9,61.7C468.6,170.8,468.6,304.4,386.5,386.5z"/> <path d="M342.3,132.9c-5.3-5.3-13.8-5.3-19.1,0l-85.6,85.6L152,132.9c-5.3-5.3-13.8-5.3-19.1,0c-5.3,5.3-5.3,13.8,0,19.1l85.6,85.6l-85.6,85.6c-5.3,5.3-5.3,13.8,0,19.1c2.6,2.6,6.1,4,9.5,4s6.9-1.3,9.5-4l85.6-85.6l85.6,85.6c2.6,2.6,6.1,4,9.5,4c3.5,0,6.9-1.3,9.5-4c5.3-5.3,5.3-13.8,0-19.1l-85.4-85.6l85.6-85.6C347.6,146.7,347.6,138.2,342.3,132.9z"/> </svg>',
-    };
-
-    NY.FilePlugin.config.messages = {
-      fileSizeError: "Tekli dosya seçim boyutunu aştınız",
-      totalFileSizeError: "Maximum yükleyebileceğiniz boyutu aştınız",
-      extensionError: "Seçtiğiniz dosyaların uzantısı desteklenmemektedir.",
-    };
   };
 })(window);
